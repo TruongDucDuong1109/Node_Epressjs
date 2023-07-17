a0d13633652b2be24b63a3e821be630517fbe0be
truongducduong <20521227@gm.uit.edu.vn>
2023-07-17 19:50:42 +0700

    update-static file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const morgan = require('morgan')
+const {engine} = require('express-handlebars')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => res.send(' Hell word '))
+//template engine
+app.engine('hbs', engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'))
+console.log(path.join(__dirname, 'resource/views'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('combined'))
+
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.get('/detal', (req,res) => {
+    res.render('detal')
+})
 
 app.listen(port, ()=> {
     console.log(`Example app listening on port ${port}`)
